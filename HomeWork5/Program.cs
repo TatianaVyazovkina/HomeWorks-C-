bff0e7ccdae10ec17c25a4ccd8c51ac5cfcdf275
Tatiana Vyazovkina <tvyazovkina@yandex.ru>
2022-11-21 23:00:08 +0400

    home work correct

--- HomeWork5/Program.cs
+++ HomeWork5/Program.cs
@@ -2,7 +2,7 @@
 // Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
 // Напишите программу, которая покажет количество чётных чисел в массиве.
 // [345, 897, 568, 234] -> 2
-
+/*
 int[] CreateRandomArray(int size, int minValue, int maxValue)
 {
     int[] newArray = new int[size];
@@ -20,6 +20,7 @@ void ShowArray(int[] array)
 
     Console.WriteLine();
 }
+*/
 /*
 int CounterEvenNumber(int[] array)
 {
@@ -44,7 +45,6 @@ int result = CounterEvenNumber(myArray);
 ShowArray(myArray);
 Console.WriteLine("Number of even array elements is " + result);
 */
-
 //Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
 //[3, 7, 23, 12] -> 19; [-4, -6, 89, 6] -> 0
 /*
