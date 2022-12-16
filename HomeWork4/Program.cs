6f5642fb8c358a850c80d14075b1cd72ffdb01b9
Tatiana Vyazovkina <tvyazovkina@yandex.ru>
2022-12-16 10:46:50 +0400

    add Home Work 6

--- HomeWork4/Program.cs
+++ HomeWork4/Program.cs
@@ -1,4 +1,4 @@
-﻿/*
+﻿
 //Задача 25. Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
 //3, 5 -> 243 (3⁵); 2, 4 -> 16
 double ExponentNumber(int a, int b)
@@ -13,7 +13,8 @@ int b = Convert.ToInt32(Console.ReadLine());
 
 double exponNumber = ExponentNumber(a, b);
 Console.WriteLine($"Exponent {b} of number {a} is {exponNumber}");
-*/
+
+
 /*
 //Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
 // 452 -> 11; 82 -> 10; 9012 -> 12
